7663519b26b2138d4d124c5950ba056bcfe9a038
LaQuangVinh <zlatanvinh2k4@gmail.com>
2023-12-20 23:54:59 +0700

    validate tầng model

--- src/models/boardModel.js
+++ src/models/boardModel.js
@@ -3,3 +3,20 @@
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
+import Joi from 'joi'
+
+const BOARD_COLLECTION_NAME = 'boards'
+const BOARD_COLLECTION_SCHEMA = Joi({
+  title: Joi.string().required().min(3).max(50).trim().strict(),
+  slug: Joi.string().required().min(3).trim().strict(),
+  description: Joi.string().required().min(3).max(255).trim().strict(),
+  columnOrderIds: Joi.array().items(Joi.string()).default([]),
+  createAt: Joi.date().timestamp('javascript').default(Date.now),
+  updateAt: Joi.date().timestamp('javascript').default(null),
+  _destroy: Joi.boolean().default(false)
+})
+
+export const boardModel = {
+  BOARD_COLLECTION_NAME,
+  BOARD_COLLECTION_SCHEMA
+}
